11deff12f59bbafed97fb6cc05e7ebdf695b17a8
James Aspinall <aspin@me.com>
2019-04-24 14:59:32 +1000

    logic to check for login and home page plus styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-    def home
-        @dogs = Dog.all
-        @owners = Owner.all
-        @users = User.all
+  def home
+    @dogs = Dog.all
+    @owners = Owner.all
+    @users = User.all
+
+    if @current_user.present?
+      render :home
+    else
+      redirect_to :login
     end
+  end
 end
